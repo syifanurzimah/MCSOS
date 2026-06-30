82fccdf379112085f1381273f828e41cb65f12a1
Syifa Nurzimah <syifanurzimah@gmail.com>
2026-06-30 09:55:29 +0700

    M4 add x86_64 IDT and exception trap path

--- kernel/core/kmain.c
+++ kernel/core/kmain.c
@@ -1,5 +1,6 @@
 #include <stdint.h>
 #include <mcsos/arch/cpu.h>
+#include <mcsos/arch/idt.h>
 #include <mcsos/kernel/log.h>
 #include <mcsos/kernel/panic.h>
 #include <mcsos/kernel/version.h>
@@ -7,10 +8,13 @@
 extern char __kernel_start[];
 extern char __kernel_end[];
 
-static void m3_selftest(void) {
+static void m4_selftest(void) {
     KERNEL_ASSERT(__kernel_end > __kernel_start);
     KERNEL_ASSERT(sizeof(uintptr_t) == 8u);
-    log_writeln("[M3] selftest: basic invariants passed");
+    KERNEL_ASSERT(sizeof(x86_64_idt_entry_t) == 16u);
+    KERNEL_ASSERT(x86_64_idt_base_for_test() != 0u);
+    KERNEL_ASSERT(x86_64_idt_limit_for_test() == 4095u);
+    log_writeln("[M4] selftest: IDT invariants passed");
 }
 
 void kmain(void) {
@@ -23,14 +27,22 @@ void kmain(void) {
     log_writeln(" kernel entered");
     log_key_value_hex64("kernel_start", (uint64_t)(uintptr_t)__kernel_start);
     log_key_value_hex64("kernel_end", (uint64_t)(uintptr_t)__kernel_end);
-    log_key_value_hex64("rflags", cpu_read_rflags());
-    m3_selftest();
+    log_key_value_hex64("rflags_before_idt", cpu_read_rflags());
 
-#ifdef MCSOS_M3_TRIGGER_PANIC
-    KERNEL_PANIC("intentional M3 panic test", 0x4D43534F533033u);
+    x86_64_idt_init();
+    m4_selftest();
+
+#ifdef MCSOS_M4_TRIGGER_BREAKPOINT
+    log_writeln("[M4] triggering intentional breakpoint exception");
+    x86_64_trigger_breakpoint_for_test();
+    log_writeln("[M4] returned from breakpoint handler");
+#endif
+
+#ifdef MCSOS_M4_TRIGGER_PANIC
+    KERNEL_PANIC("intentional M4 panic test", 0x4D43534F533034u);
 #else
-    log_writeln("[M3] panic path installed; intentional panic disabled");
-    log_writeln("[M3] ready for QEMU smoke test and GDB audit");
+    log_writeln("[M4] IDT and exception dispatch path installed");
+    log_writeln("[M4] ready for QEMU smoke test and GDB audit");
     cpu_halt_forever();
 #endif
 }
